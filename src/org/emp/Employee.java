c28d0d395d36ede80ddcba7d2feb71da1aa66040
gowtham24008 <gowtham24008@gmail.com>
2022-03-10 10:53:31 +0530

    task given

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,8 @@
+package org.emp;
+
+public class Employee {
+	public void empName() {
+		// TODO Auto-generated method stub
+System.out.println("emp name is gowtham");
+	}
+}
---
+++ src/org/project/Project.java
@@ -0,0 +1,20 @@
+package org.project;
+import org.emp.Employee;
+import org.company.Company;
+import org.client.Client;
+public class Project {
+	private void projectName() {
+		// TODO Auto-generated method stub
+System.out.println("project name is lolipop");
+	}
+	public static void main(String[] args) {
+		Project m = new Project();
+		m.projectName();
+		Employee n = new Employee();
+		n.empName();
+		Company b = new Company();
+		b.companyName();
+		Client v = new Client();
+		v.clientName();
+	}
+}
